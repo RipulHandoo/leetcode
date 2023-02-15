b4b35702f4dc11ad814e2bafb363495a50605723
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-16 02:44:12 +0530

    Update Solution.java

--- Algorithms/7. Reverse Integer/Solution.java
+++ Algorithms/7. Reverse Integer/Solution.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public int reverse(int x) {
+        int copy = x;
+        long rev = 0;
+        int n = 0;
+        while(copy != 0) {
+            n = copy%10;
+            rev = rev*10 + n;
+            copy = copy/10;
+        }
+        if(rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE) {
+            return 0;
+        }
+        if(copy < 0) {
+            rev = -rev;
+            return (int)rev;
+        }
+        return (int)rev;
+    }
+}
