5c19bfc2edad46b067439f771f81f6c8f542d57d
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-18 16:07:43 +0530

    Update Solution.java

--- Algorithms/9. Palindrome Number/Solution.java
+++ Algorithms/9. Palindrome Number/Solution.java
@@ -1,1 +1,24 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+        int copy = x;
+        int rev = 0;
+        int n = 0;
+//       if the number is smaller than zero simply return false.
+        if(x < 0) {
+            return false;
+        }
+      
+//       revrese the number to compare with x, if both are equal return true else return false
+        while(copy != 0) {
+            n = copy%10;
+            rev = rev*10 + n;
+            copy = copy/10;
+        }
+        if(rev == x) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
