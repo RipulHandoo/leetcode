0239349e64966e07b8c625ab0d08b4c930028a29
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-02-18 16:03:30 +0530

    Update Solution.java
    This problem can be solved in O(n^2) by using nested for loops and checking for each array element.

--- Algorithms/1. Two Sum/Solution.java
+++ Algorithms/1. Two Sum/Solution.java
@@ -1,1 +1,18 @@
+// this problem is solved uisng HASHMAP to improve the code time complexity from O(n^2) to O(n)
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0;i<nums.length;i++) {
+            if(map.containsKey(target - nums[i])) {
+                arr[0] = map.get(target-nums[i]);
+                arr[1] = i;
+            }
+            if(!map.containsKey(nums[i])) {
+                map.put(nums[i],i);
+            }
+        }
+        return arr;
+    }
+}
